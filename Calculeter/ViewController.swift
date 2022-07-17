ff2fdcba053493c4d621fbcd757551a0eab33ef4
Ержан Джанарстанов <erzandzanarstanov@MacBook-Pro-Erzan.local>
2022-07-17 11:24:30 +0600

    Initial commit

---
+++ Calculeter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculeter
+//
+//  Created by Ержан Джанарстанов on 15.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
